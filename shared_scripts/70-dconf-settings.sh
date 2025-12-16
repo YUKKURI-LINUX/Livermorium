9a2fd6c470e448f7d582981a73f9869305ffeb15
pokapokalinux <pokapokalinux.gmail.com>
2025-12-16 19:15:42 +0900

    Minimum gnome

--- shared_scripts/70-dconf-settings.sh
+++ shared_scripts/70-dconf-settings.sh
@@ -4,10 +4,10 @@ set -e
 source /tmp/env.sh
 
 SCRIPT_NAME="$(basename "$0")"
-echo "[$SCRIPT_NAME] Setting up dconf default values..."
+#echo "[$SCRIPT_NAME] Setting up dconf default values..."
 
 # The file /etc/dconf/profile/user is required
-echo "user-db:user\nsystem-db:local" > /etc/dconf/profile/user
+#echo "user-db:user\nsystem-db:local" > /etc/dconf/profile/user
 
 # Skip if config file doesn't exist
 # Note: The original condition [ -f /etc/dconf/db/local.d/* ] is slightly incorrect for globbing files; 
@@ -18,6 +18,9 @@ if  [ -f  /etc/dconf/db/local.d/* ]; then
     # dconf update
     if command -v dconf >/dev/null 2>&1; then
         dconf update
+        dconf dump / > /tmp/dump.txt
+    else
+        echo "[$SCRIPT_NAME] Skipped dconf settings"
     fi
 
     echo "[$SCRIPT_NAME] Finished dconf settings successfully"
